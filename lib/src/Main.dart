3f615c077c50f9d8f0c4069ec1ba17431452b1e7
Vladimir Minkin <Vladimir.Minkin@massive.co>
2019-09-25 11:00:45 +0200

    Comments update accroding to dart style recommendations

--- lib/src/Main.dart
+++ lib/src/Main.dart
@@ -4,39 +4,34 @@ part 'model/State.dart';
 part 'model/Action.dart';
 
 class DartMachine extends IDartMachine {
-	/**
-	 * Create a state machine and populate with states
-	 */
-	DartMachine() {
-	}
+	/// Create a state machine and populate with states
+	DartMachine();
 
 	List<Action> _actions = new List<Action>();
 	List<State> _states = new List<State>();
-	State _currentState = null;
+	State _currentState;
 
-	/**
-	 * Does an action exist in the state machine?
-	 * @param action The action in question.
-	 * @return True if the action exists, false if it does not.
-	 */
+	/// Does an action exist in the state machine?
+	///
+	/// @param action The action in question.
+	/// @return True if the action exists, false if it does not.
 	bool actionExists( String checkAction ) {
 		return ( _findState( checkAction ) != null );
 	}
 
-	/**
-	 * Add a valid link between two states.
-	 * The state machine can then move between
-	 * @param fromState State you want to move from.
-	 * @param toState State you want to move to.
-	 * @param action Action that when performed will move from the from state to the to state.
-	 * @param handler Optional method that gets called when moving between these two states.
-	 * @return true if link was added, false if it was not.
-	 */
+
+	/// Add a valid link between two states. The state machine can then move between
+	///
+	/// @param fromState State you want to move from.
+	/// @param toState State you want to move to.
+	/// @param action Action that when performed will move from the from state to the to state.
+	/// @param handler Optional method that gets called when moving between these two states.
+	/// @return true if link was added, false if it was not.
 	bool addAction( String fromState, String toState, String action, [ Function handler = null ]) {
 		State from;
 		State to;
 
-		// can't have duplicate actions
+		/// can't have duplicate actions
 		for ( Action check in _actions )
 			if ( check.fromState.name == fromState && check.name == action )
 				return false;
@@ -57,28 +52,26 @@ class DartMachine extends IDartMachine {
 		return true;
 	}
 
-	/**
-	 * Adds a new state to the state machine.
-	 * @param newState The new state to add.
-	 * @return True is teh state was added, false if it was not.
-	 */
+	/// Adds a new state to the state machine.
+	///
+	/// @param newState The new state to add.
+	/// @return True is teh state was added, false if it was not.
 	bool addState( String newState ) {
-		// can't have duplicate states
+		/// can't have duplicate states
 		if ( stateExists( newState )) return false;
 
 		_states.add( new State( newState ));
 
-		// if no states exist set current state to first state
+		/// if no states exist set current state to first state
 		if ( _states.length == 1 ) _currentState = _states[0];
 
 		return true;
 	}
 
-	/**
-	 * Move from the current state to another state.
-	 * @param toState New state to try and move to.
-	 * @return True if the state machine has moved to this new state, false if it was unable to do so.
-	 */
+	/// Move from the current state to another state.
+	///
+	/// @param toState New state to try and move to.
+	/// @return True if the state machine has moved to this new state, false if it was unable to do so.
 	bool changeState( String toState ) {
 		if ( !stateExists( toState )) return false;
 
@@ -93,17 +86,15 @@ class DartMachine extends IDartMachine {
 		return false;
 	}
 
-	/**
-	 * What is the current state?
-	 * @return The current state.
-	 */
+	/// What is the current state?
+	///
+	/// @return The current state.
 	String currentState() { return _currentState != null ? _currentState.name : null; }
 
-/**
- * Change the current state by performing an action.
- * @param action The action to perform.
- * @return True if the action was able to be performed and the state machine moved to a new state, false if the action was unable to be performed.
- */
+	/// Change the current state by performing an action.
+	///
+	/// @param action The action to perform.
+	/// @return True if the action was able to be performed and the state machine moved to a new state, false if the action was unable to be performed.
 	bool performAction( String actionName ) {
 		for ( Action action in _actions )
 			if ( action.fromState == _currentState && actionName == action.name ) {
@@ -116,16 +107,13 @@ class DartMachine extends IDartMachine {
 		return false;
 	}
 
-	/**
-	 * Go back to the initial starting state
-	 */
+	/// Go back to the initial starting state
 	void reset() { _currentState = _states.length > 0 ? _states[0] : null; }
 
-	/**
-	 * Does a state exist?
-	 * @param state The state in question.
-	 * @return True if the state exists, false if it does not.
-	 */
+	/// Does a state exist?
+	///
+	/// @param state The state in question.
+	/// @return True if the state exists, false if it does not.
 	bool stateExists( String checkState ) {
 		for ( State state in _states )
 			if ( checkState == state.name )
@@ -134,10 +122,10 @@ class DartMachine extends IDartMachine {
 		return false;
 	}
 
-	/**
-	 * What are the valid actions you can perform from the current state?
-	 * @return An array of actions.
-	 */
+
+	/// What are the valid actions you can perform from the current state?
+	///
+	/// @return An array of actions.
 	List<Action> validActions() {
 		List<Action> actions = [];
 		for ( Action action in _actions )
@@ -147,10 +135,9 @@ class DartMachine extends IDartMachine {
 		return actions;
 	}
 
-	/**
-	 * What are the valid states you can get to from the current state?
-	 * @return An array of states.
-	 */
+	/// What are the valid states you can get to from the current state?
+	///
+	/// @return An array of states.
 	List<State> validStates() {
 		List<State> states = [];
 		for ( Action action in _actions )
--- lib/src/model/Action.dart
+++ lib/src/model/Action.dart
@@ -1,38 +1,37 @@
 part of dart_machine;
 
-//------------------------------------------------------------------------------
-//
-// Copyright (c) 2018 Vladimir Cores (Minkin) @ LOGICO Technologies (https://logico.tech)
-//
-// Permission is hereby granted, free of charge, to any person obtaining a copy
-// of this software and associated documentation files (the "Software"), to deal
-// in the Software without restriction, including without limitation the rights
-// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-// copies of the Software, and to permit persons to whom the Software is
-// furnished to do so, subject to the following conditions:
-//
-// The above copyright notice and this permission notice shall be included in
-// all copies or substantial portions of the Software.
-//
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-// THE SOFTWARE.
-//
-//------------------------------------------------------------------------------
+///------------------------------------------------------------------------------
+///
+/// Copyright (c) 2018 Vladimir Cores (Minkin) @ LOGICO Technologies (https://logico.tech)
+///
+/// Permission is hereby granted, free of charge, to any person obtaining a copy
+/// of this software and associated documentation files (the "Software"), to deal
+/// in the Software without restriction, including without limitation the rights
+/// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+/// copies of the Software, and to permit persons to whom the Software is
+/// furnished to do so, subject to the following conditions:
+///
+/// The above copyright notice and this permission notice shall be included in
+/// all copies or substantial portions of the Software.
+///
+/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+/// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+/// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+/// THE SOFTWARE.
+///
+///------------------------------------------------------------------------------
 
 class Action
 {
-	/**
-	 * Creates a new action. The action method is optional.
-	 * @param fromState State to move form.
-	 * @param toState State to move to.
-	 * @param name Action's name.
-	 * @param action Method to call on performing action.
-	 */
+	/// Creates a new action. The action method is optional.
+	///
+	/// @param fromState State to move form.
+	/// @param toState State to move to.
+	/// @param name Action's name.
+	/// @param action Method to call on performing action.
 	Action( State fromState, State toState, String name, [ Function action = null ]) {
 		_fromState = fromState;
 		_toState = toState;
@@ -45,30 +44,22 @@ class Action
 	String _name;
 	Function _action;
 
-	/**
-	 * @return The method to call on preforming the action.
-	 */
+	/// @return The method to call on preforming the action.
 	Function get action {
 		return _action;
 	}
 
-	/**
-	 * @return The state to move from.
-	 */
+	/// @return The state to move from.
 	State get fromState {
 		return _fromState;
 	}
 
-	/**
-	 * @return The action's name.
-	 */
+	/// @return The action's name.
 	String get name {
 		return _name;
 	}
 
-	/**
-	 * @return  The state to move to.
-	 */
+	/// @return  The state to move to.
 	State get toState {
 		return _toState;
 	}
